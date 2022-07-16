5bec2860195e0ac792974e4d1b1f04a0277e1508
Dzhegash <dzhegash@gmail.com>
2022-07-16 05:57:36 +0500

    Added structs `CmcCategories, Status, Category` to`categories.rs`

---
+++ src/api/cryptocurrency/categories.rs
@@ -0,0 +1,33 @@
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct CmcCategories {
+    pub status: Status,
+    pub data: Vec<Category>,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct Status {
+    pub timestamp: String,
+    pub error_code: i64,
+    pub error_message: Value,
+    pub elapsed: i64,
+    pub credit_count: i64,
+    pub notice: Value,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct Category {
+    pub id: String,
+    pub name: String,
+    pub title: String,
+    pub description: String,
+    pub num_tokens: i64,
+    pub avg_price_change: f64,
+    pub market_cap: f64,
+    pub market_cap_change: f64,
+    pub volume: f64,
+    pub volume_change: f64,
+    pub last_updated: String,
+}
