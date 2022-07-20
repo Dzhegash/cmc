c5cd67ae50241ced39fc0f3b33ec66ef7cb4d5c3
Dzhegash <dzhegash@gmail.com>
2022-07-20 10:19:55 +0500

    Added `impl Display` for `CmcCategories`

--- src/api/cryptocurrency/categories.rs
+++ src/api/cryptocurrency/categories.rs
@@ -1,5 +1,6 @@
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
+use std::fmt::{Display, Formatter};
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct CmcCategories {
@@ -30,4 +31,17 @@ pub struct Category {
     pub volume: f64,
     pub volume_change: f64,
     pub last_updated: String,
+}
+
+impl Display for CmcCategories {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        for cc in &self.data {
+            let _ = writeln!(
+                f,
+                "Name: {}\nAvg. Price Change: {}\nMarket Cap: {}\nVolume: {}\n-----------------------------",
+                cc.name, cc.avg_price_change, cc.market_cap, cc.volume
+            );
+        }
+        Ok(())
+    }
 }
