ca47340eea701a4f32203e4f436f5f5933cf0b3b
Dzhegash <dzhegash@gmail.com>
2022-07-22 09:22:53 +0500

    Added `Status` to `category.rs`

---
+++ src/api/cryptocurrency/category.rs
@@ -0,0 +1,12 @@
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct Status {
+    pub timestamp: String,
+    pub error_code: i64,
+    pub error_message: Value,
+    pub elapsed: i64,
+    pub credit_count: i64,
+    pub notice: Value,
+}
