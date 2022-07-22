9c0c9298e0151485993e90c8631850c6bc9e94a4
Dzhegash <dzhegash@gmail.com>
2022-07-22 09:25:18 +0500

    Added `Category` to `category.rs`

--- src/api/cryptocurrency/category.rs
+++ src/api/cryptocurrency/category.rs
@@ -10,3 +10,19 @@ pub struct Status {
     pub credit_count: i64,
     pub notice: Value,
 }
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct Category {
+    pub id: String,
+    pub name: String,
+    pub title: String,
+    pub description: String,
+    pub num_tokens: i64,
+    pub last_updated: String,
+    pub avg_price_change: f64,
+    pub market_cap: f64,
+    pub market_cap_change: f64,
+    pub volume: f64,
+    pub volume_change: f64,
+    pub coins: Vec<Coin>,
+}
