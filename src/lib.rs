f31907a136c34c0010dcbb8482ccb5311bbb0327
Dzhegash <dzhegash@gmail.com>
2022-07-30 14:35:04 +0500

    Doc `lib.rs` update

--- src/lib.rs
+++ src/lib.rs
@@ -54,8 +54,8 @@
 //!     Err(err) => println!("Error: {}", err),
 //! }
 //!
-//! // 3.2 BTC in USD (using id's)
-//! match cmc.price_conversion_id(3.2, "1", None, "2781") {
+//! // 1.6 ETH in XMR (using id's)
+//! match cmc.price_conversion_id(1.6, "1027", None, "328") {
 //!     Ok(price) => println!("Total price: {}", price),
 //!     Err(err) => println!("Error: {}", err),
 //! }
