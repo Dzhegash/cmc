68e9d1f57eba53c79536dfc304f4d5b82bd2fbfd
Dzhegash <dzhegash@gmail.com>
2022-08-02 14:08:15 +0500

    Removed `Default` implementation for CmcMetadata

--- src/api/cryptocurrency/metadata_v2.rs
+++ src/api/cryptocurrency/metadata_v2.rs
@@ -2,7 +2,7 @@ use std::collections::HashMap;
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
 
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct CmcMetadata {
     pub status: Status,
     pub data: HashMap<String, Metadata>,
