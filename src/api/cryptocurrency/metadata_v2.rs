65f15285c5b97c7c811932fe457e6db54b2fa980
Dzhegash <dzhegash@gmail.com>
2022-08-03 12:09:41 +0500

    Added `Status` to `metadata_v2.rs`

--- src/api/cryptocurrency/metadata_v2.rs
+++ src/api/cryptocurrency/metadata_v2.rs
@@ -8,3 +8,12 @@ pub struct CmcMetadata {
     pub data: HashMap<String, Metadata>,
 }
 
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct Status {
+    pub timestamp: String,
+    pub error_code: i64,
+    pub error_message: Value,
+    pub elapsed: i64,
+    pub credit_count: i64,
+    pub notice: Value,
+}
