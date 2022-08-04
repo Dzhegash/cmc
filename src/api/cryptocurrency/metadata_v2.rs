35abd2fbce0ae7a144f5590854f9bef2dfdc6de3
Dzhegash <dzhegash@gmail.com>
2022-08-04 11:29:56 +0500

    Added `Metadata` to `metadata_v2.rs`

--- src/api/cryptocurrency/metadata_v2.rs
+++ src/api/cryptocurrency/metadata_v2.rs
@@ -1,6 +1,6 @@
-use std::collections::HashMap;
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
+use std::collections::HashMap;
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct CmcMetadata {
@@ -16,4 +16,32 @@ pub struct Status {
     pub elapsed: i64,
     pub credit_count: i64,
     pub notice: Value,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct Metadata {
+    pub id: i64,
+    pub name: String,
+    pub symbol: String,
+    pub category: String,
+    pub description: String,
+    pub slug: String,
+    pub logo: String,
+    pub subreddit: String,
+    pub notice: String,
+    pub tags: Vec<String>,
+    #[serde(rename = "tag-names")]
+    pub tag_names: Vec<String>,
+    #[serde(rename = "tag-groups")]
+    pub tag_groups: Vec<String>,
+    pub urls: Urls,
+    pub platform: Option<Platform>,
+    pub date_added: String,
+    pub twitter_username: String,
+    pub is_hidden: i64,
+    pub date_launched: Value,
+    pub contract_address: Vec<Value>,
+    pub self_reported_circulating_supply: Value,
+    pub self_reported_tags: Value,
+    pub self_reported_market_cap: Value,
 }
