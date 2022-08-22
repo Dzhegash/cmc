aefde3c227ffe42a81169b8de2602a96b233c951
Dzhegash <dzhegash@gmail.com>
2022-08-23 04:22:09 +0500

    Removed `Default` implementation for `api::exchange::metadata::Urls`

--- src/api/exchange/metadata.rs
+++ src/api/exchange/metadata.rs
@@ -1,7 +1,7 @@
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
 
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct Urls {
     pub chat: Vec<Value>,
     pub twitter: Vec<Value>,
