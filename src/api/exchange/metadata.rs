e4489a71627b7c00d098ed52e67c964eff0894c3
Dzhegash <dzhegash@gmail.com>
2022-08-25 05:01:05 +0500

    Added `api::exchange::metadata::Status`

--- src/api/exchange/metadata.rs
+++ src/api/exchange/metadata.rs
@@ -2,6 +2,16 @@ use serde::{Deserialize, Serialize};
 use serde_json::Value;
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct Status {
+    pub timestamp: String,
+    pub error_code: i64,
+    pub error_message: Value,
+    pub elapsed: i64,
+    pub credit_count: i64,
+    pub notice: Value,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct Exchange {
     pub id: i64,
     pub name: String,
