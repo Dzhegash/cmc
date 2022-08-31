30c95bffd433222e5e8e447a8f2ce8552d6fa5b4
Dzhegash <dzhegash@gmail.com>
2022-08-31 08:20:02 +0500

    Added `api::exchange::id_map::Status`

--- src/api/exchange/id_map.rs
+++ src/api/exchange/id_map.rs
@@ -1,6 +1,17 @@
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
 
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct Status {
+    pub timestamp: String,
+    pub error_code: i64,
+    pub error_message: Value,
+    pub elapsed: i64,
+    pub credit_count: i64,
+    pub notice: Value,
+}
+
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
 pub struct Exchange {
     pub id: i64,
