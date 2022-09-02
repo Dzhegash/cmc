e5cd6c260734a241b1952bc5e806cb8577e432b1
Dzhegash <dzhegash@gmail.com>
2022-09-02 12:13:03 +0500

    Added `quotes_latest_by_slug()`

--- src/api.rs
+++ src/api.rs
@@ -414,6 +414,34 @@ impl Cmc {
         }
     }
 
+    pub fn quotes_latest_by_slug<T: Into<String>>(&self, slugs: T) -> CmcResult<QLv2Slug> {
+        let slugs = slugs.into();
+
+        let rb = self
+            .add_endpoint("v2/cryptocurrency/quotes/latest")
+            .query(&[("slug", slugs)]);
+
+        let resp = if let Some(currency_id) = &self.config.currency_id {
+            rb.query(&[("convert_id", currency_id)]).send()?
+        } else {
+            rb.query(&[("convert", &self.config.currency)]).send()?
+        };
+
+        match resp.status() {
+            StatusCode::OK => {
+                let root = resp.json::<QLv2Slug>()?;
+                Ok(root)
+            }
+            code => {
+                let root = resp.json::<ApiError>()?;
+                Err(CmcErrors::ApiError(format!(
+                    "Status Code: {}. Error message: {}",
+                    code, root.status.error_message
+                )))
+            }
+        }
+    }
+
     /// Returns API key details and usage stats.
     pub fn key_info(&self) -> CmcResult<KeyInfo> {
         let resp = self.add_endpoint("v1/key/info").send()?;
